6a791c81ac8cb218dd31f14154e45eafac17fcb4
tingkatin <info.tingkatin@gmail.com>
2023-07-01 14:50:04 +0700

    Added user input and calculate method

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,29 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
+    private static int calculate(double rental, double price) {
+
+        double result = ((rental * 12) / price) * 100;
+
+        return (int) result;
+
+    }
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("$$$$RENTAL YIELD CALCULATOR$$$$");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.print("Enter monthly income (IDR): ");
+        double rental = scanner.nextDouble();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.print("Enter property price (IDR): ");
+        double price = scanner.nextDouble();
+
+        int result = calculate(rental, price);
+
+        System.out.println("Your Rental Annual Yield: " + result + "%");
+
     }
 }
